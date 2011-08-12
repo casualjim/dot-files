bd3bff6601d11bc11997a84d97c1aef1b5a649da
Ivan Porto Carrero <ivan@flanders.co.nz>
2011-08-12 13:34:58 +0100

    Adds oh-my-zsh to the list

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-rm -rf ~/.vimrc ~/.vim ~/.emacs.d
+rm -rf ~/.vimrc ~/.vim ~/.emacs.d ~/.oh-my-zsh ~/.zshrc
 curr_dir=$(cd `dirname $0` && pwd)
 ln -sf ${curr_dir}/vimfiles ~/.vim
 ln -sf ${curr_dir}/_vimrc ~/.vimrc
 ln -sf ${curr_dir}/vim-addons ~/.vim-addons
 ln -sf ${curr_dir}/emacs.d ~/.emacs.d
+ln -sf ${curr_dir}/oh-my-zsh ~/.oh-my-zsh
+cp ${curr_dir}/oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
-echo "Vim and Emacs have been configured."
+echo "Environment has been configured."
