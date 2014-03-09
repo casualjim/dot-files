f0cd5052a12ef9e3cb2ce32d37beb48d3dab0fdf
Ivan Porto Carrero <ivan@flanders.co.nz>
2014-03-09 14:38:39 -0700

    a few more tweaks

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ rm -rf ~/.vimrc ~/.vim ~/.emacs.d ~/.oh-my-zsh ~/.zshrc
 curr_dir=$(cd `dirname $0` && pwd)
 ln -sf ${curr_dir}/vimreboot ~/.vim
 ln -sf ${curr_dir}/vimreboot/vimrc ~/.vimrc
+ln -sf ${curr_dir}/ctags ~/.ctags
 # ln -sf ${curr_dir}/vim-addons ~/.vim-addons
 # ln -sf ${curr_dir}/emacs.d ~/.emacs.d
 ln -sf ${curr_dir}/oh-my-zsh ~/.oh-my-zsh
