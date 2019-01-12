cab15e4c68a9de324c03d35d2236167ea63a554b
Ivan Porto Carrero <ivan@flanders.co.nz>
2019-01-12 10:50:38 -0800

    Update infect.sh

--- infect.sh
+++ infect.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-git clone --recursive https://github.com/casualjim/dot-files $HOME/dot-files
-cd $HOME/dot-files
+mkdir -p $HOME/github/casualjim/dot-files
+git clone --recursive https://github.com/casualjim/dot-files $HOME/github/casualjim/dot-files
+cd $HOME/github/casualjim/dot-files
 ./setup.sh
