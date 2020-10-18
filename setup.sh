d7cd53aca42da9ec22b9a0aa5276934c137b77ee
Ivan Porto Carrero <ivan@flanders.co.nz>
2020-10-18 08:40:53 -0700

    fix alpine devenv build

--- setup.sh
+++ setup.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 mkdir -p ~/.config
 rm -rf ~/.vimrc ~/.vim ~/.antigen ~/.emacs.d ~/.oh-my-zsh ~/.zshrc ~/.config/nvim
-curr_dir=$(cd `dirname $0` && pwd)
-ln -sf ${curr_dir}/vimreboot ~/.vim
-ln -sf ${curr_dir}/vimreboot/init.vim ~/.vimrc
-ln -sf ${curr_dir}/ctags ~/.ctags
-ln -sf ${curr_dir}/zshrc ~/.zshrc
-ln -sf ${curr_dir}/.tmux.conf ~/.tmux.conf
-ln -sf ${curr_dir}/gitconfig ~/.gitconfig
-ln -sf ${curr_dir}/nvim ~/.config/nvim
+curr_dir=$(cd "$(dirname "${0}")" && pwd)
+ln -sf "${curr_dir}/vimreboot" ~/.vim
+ln -sf "${curr_dir}/vimreboot/init.vim" ~/.vimrc
+ln -sf "${curr_dir}/ctags" ~/.ctags
+ln -sf "${curr_dir}/zshrc" ~/.zshrc
+ln -sf "${curr_dir}/.tmux".conf ~/.tmux.conf
+ln -sf "${curr_dir}/gitconfig" ~/.gitconfig
+ln -sf "${curr_dir}/nvim" ~/.config/nvim
+ln -sf "${curr_dir}/starship.toml" ~/.config/starship.toml
 
 NPM="npm"
 GEM="gem"
 GO=""
 echo "Installing YouCompleteMe dependencies"
-if [ `uname` = 'Darwin' ]; then
+
+if [ "$(uname)" = 'Darwin' ]; then
   brew install cmake nodejs hub diff-so-fancy httpie tmux ripgrep exa prettyping bat
 fi
+
 if [ -f /etc/os-release ]; then
+  # shellcheck disable=SC1091
   . /etc/os-release
   if [ "${ID_LIKE-$ID}" = "debian" ]; then
     echo "Installing for debian"
     sudo apt-get install -y curl pv httpie vim-nox zsh cmake python-dev clang libclang-dev tmux exuberant-ctags ncurses-term nodejs npm direnv ruby jq git-hub iputils-ping gawk
     sudo ln -sf /usr/bin/nodejs /usr/bin/node
-    if [ -z `which go` ]; then
-      curl -sL https://dl.google.com/go/$(curl --silent https://golang.org/doc/devel/release.html | grep -Eo 'go[0-9]+(\.[0-9]+)+' | sort -V | uniq | tail -1).linux-amd64.tar.gz | sudo tar -C /usr/local -xz
+    if [ -z "$(which go)" ]; then
+      curl -sL "https://dl.google.com/go/$(curl --silent https://golang.org/doc/devel/release.html | grep -Eo 'go[0-9]+(\.[0-9]+)+' | sort -V | uniq | tail -1).linux-amd64.tar.gz" | sudo tar -C /usr/local -xz
+      # shellcheck disable=SC2016
       echo 'export PATH="/usr/local/go/bin:$PATH"' | sudo tee /etc/profile.d/golang.sh
+      # shellcheck disable=SC1091
       source /etc/profile.d/golang.sh
     fi
     sudo curl -o /usr/bin/prettyping -L --progress https://raw.githubusercontent.com/denilsonsa/prettyping/master/prettyping
     sudo chmod +x /usr/bin/prettyping
-    curl -o /tmp/bat.deb -L --progress $(curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | jq -r '.assets[] | select(.name | contains("amd64.deb") and contains("bat_")) | .browser_download_url')
+    curl -o /tmp/bat.deb -L --progress "$(curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | jq -r '.assets[] | select(.name | contains("amd64.deb") and contains("bat_")) | .browser_download_url')"
     sudo dpkg -i /tmp/bat.deb
     mkdir -p /tmp/exa
-    curl -o /tmp/exa/exa.zip -L --progress $(curl -s https://api.github.com/repos/ogham/exa/releases/latest | jq -r '.assets[] | select(.name | contains("exa-linux")) | .browser_download_url')
+    curl -o /tmp/exa/exa.zip -L --progress "$(curl -s https://api.github.com/repos/ogham/exa/releases/latest | jq -r '.assets[] | select(.name | contains("exa-linux")) | .browser_download_url')"
+    # shellcheck disable=SC2164
     pushd /tmp/exa
     unzip -d . exa.zip
     sudo mv exa-linux-x86_64 /usr/bin/exa
+    # shellcheck disable=SC2164
     popd
     rm -rf /tmp/exa
     echo "deb http://packages.cloud.google.com/apt cloud-sdk-cosmic main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
@@ -54,29 +62,6 @@ if [ -f /etc/os-release ]; then
     echo "Installing for arch"
     sudo pacman -S cmake python clang go tmux ctags ncurses nodejs diff-so-fancy exa ripgrep bat prettyping
   fi
-  if [ "${ID}" = "fedora" ]; then
-    NPM="sudo npm"
-    GEM="sudo gem"
-    echo "Installing for fedora"
-    sudo dnf install -y kernel-headers httpie cmake clang tmux ctags-etags ncurses nodejs npm vim python-devel ruby-devel
-    if [ -z `which go` ]; then
-      echo "==> installing go"
-      curl -sL https://dl.google.com/go/$(curl --silent https://golang.org/doc/devel/release.html | grep -Eo 'go[0-9]+(\.[0-9]+)+' | sort -V | uniq | tail -1).linux-amd64.tar.gz | sudo tar -C /usr/local -xz
-      echo 'export PATH="/usr/local/go/bin:$PATH"' | sudo tee /etc/profile.d/golang.sh
-    fi
-  fi
-fi
-
-if [ "${ID}" != "fedora" ]; then
-  GH_HUB_VERSION=2.10.0
-  curl -L'#' "https://github.com/github/hub/releases/download/v$GH_HUB_VERSION/hub-linux-amd64-$GH_HUB_VERSION.tar.gz" | tar -C /tmp
-  cp "/tmp/linux-hub-amd64-$GH_HUB_VERSION/hub" /usr/bin
-  chmod +x "/usr/bin/hub"
-  cp "/tmp/linux-hub-amd64-$GH_HUB_VERSION/man/hub.1" /usr/share/man/man1
-  mandb
-  mkdir -p /usr/share/bash-completion /usr/share/zsh/vendor-completions
-  cp "/tmp/linux-hub-amd64-$GH_HUB_VERSION/etc/hub/hub.bash_completion.sh" /usr/share/bash-completion/completions/hub
-  cp "/tmp/linux-hub-amd64-$GH_HUB_VERSION/etc/hub.zsh_completion" /usr/share/zsh/vendor-completions/_hub
 fi
 
 echo "Installing vim plugins"
@@ -91,15 +76,8 @@ $GEM install mdl
 echo "installing jsyaml"
 $NPM -g install js-yaml
 
-# echo "Installing jsbeautify"
-# cd ~/.vim/bundle/js-beautify
-# git submodule update --init --recursive
-
-# echo "Installing YouCompleteMe"
-# cd ~/.vim/bundle/YouCompleteMe
-# ./install.py --clang-completer --gocode-completer
-
 go get -u -v golang.org/x/tools/cmd/...
 
-cd ${curr_dir}
+# shellcheck disable=SC2164
+cd "${curr_dir}"
 echo "Environment has been configured."
